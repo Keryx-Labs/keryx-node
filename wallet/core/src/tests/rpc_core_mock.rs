9251b528f4c4b9898ef6f92124863ce76767352c
slashkrx <keryx_krx@protonmail.com>
2026-09-05 22:52:15 +0200

    ci: release workflow building keryxd and keryx-cli zips for Linux and Windows

--- wallet/core/src/tests/rpc_core_mock.rs
+++ wallet/core/src/tests/rpc_core_mock.rs
@@ -307,6 +307,22 @@ impl RpcApi for RpcCoreMock {
         Err(RpcError::NotImplemented)
     }
 
+    async fn get_utxo_entries_by_outpoints_call(
+        &self,
+        _connection: Option<&DynRpcConnection>,
+        _request: GetUtxoEntriesByOutpointsRequest,
+    ) -> RpcResult<GetUtxoEntriesByOutpointsResponse> {
+        Err(RpcError::NotImplemented)
+    }
+
+    async fn get_utxo_count_by_address_call(
+        &self,
+        _connection: Option<&DynRpcConnection>,
+        _request: GetUtxoCountByAddressRequest,
+    ) -> RpcResult<GetUtxoCountByAddressResponse> {
+        Err(RpcError::NotImplemented)
+    }
+
     async fn get_sink_blue_score_call(
         &self,
         _connection: Option<&DynRpcConnection>,
